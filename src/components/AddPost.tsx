862abe31d4fd5c4e885fd0d7788a012b6e4d0d86
Mihail Bivol <biv@yahoo.com>
2025-02-20 09:53:55 +0000

    AddPost component design

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,8 +1,35 @@
+import Image from 'next/image'
 import React from 'react'
 
 function AddPost() {
   return (
-    <div>AddPost</div>
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      <Image src={"https://images.pexels.com/photos/3140079/pexels-photo-3140079.jpeg"} width={48} height={48} alt='' className='w-12 h-12 object-cover rounded-full' />
+      <div className="flex-1">
+        <div className="flex gap-4">
+          <textarea name="" id="" placeholder="What's on your mind?" className='bg-slate-100 rounded-lg flex-1 p-2'></textarea>
+          <Image src={"/emoji.png"} width={20} height={20} alt='' className='w-5 h-5 cursor-pointer self-end' />
+        </div>
+        <div className="flex item-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src={"/addImage.png"} width={20} height={20} alt=''/>
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src={"/addVideo.png"} width={20} height={20} alt=''/>
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src={"/addevent.png"} width={20} height={20} alt=''/>
+            Event
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src={"/poll.png"} width={20} height={20} alt=''/>
+            Poll
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
