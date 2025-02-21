5ef99509c91c207b3691afdd184b1a738e459d03
Mihail Bivol <biv@yahoo.com>
2025-02-21 12:10:45 +0000

    Left menu - components design

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import React from "react";
+
+function ProfileCard() {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image
+          src={
+            "https://images.pexels.com/photos/2763964/pexels-photo-2763964.jpeg"
+          }
+          alt=""
+          fill
+          className="rounded-md object-cover"
+        />
+        <Image
+          src={
+            "https://images.pexels.com/photos/30649518/pexels-photo-30649518/free-photo-of-moody-black-and-white-portrait-of-pensive-man.jpeg"
+          }
+          alt=""
+          width={48}
+          height={48}
+          className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
+      </div>
+      <div className="h-20 flex flex-col gap-2 items-center">
+        <span className="font-semibold">Gabriel Khan</span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image
+              src="https://images.pexels.com/photos/19578755/pexels-photo-19578755/free-photo-of-woman-watching-birds-and-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image
+              src="https://images.pexels.com/photos/19578755/pexels-photo-19578755/free-photo-of-woman-watching-birds-and-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image
+              src="https://images.pexels.com/photos/19578755/pexels-photo-19578755/free-photo-of-woman-watching-birds-and-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+          </div>
+          <span className="text-xs text-gray-500">543 Followers</span>
+        </div>
+        <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+          My Profile
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
