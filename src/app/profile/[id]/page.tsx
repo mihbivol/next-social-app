5942e9f3f61ffe524154f73d5afc69ad235cf9e6
Mihail Bivol <biv@yahoo.com>
2025-02-21 12:28:35 +0000

    User profile  page - design

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,25 +1,61 @@
-import Feed from '@/components/Feed'
-import LeftMenu from '@/components/LeftMenu'
-import RightMenu from '@/components/RightMenu'
-import Stories from '@/components/Stories'
-import React from 'react'
+import Feed from "@/components/Feed";
+import LeftMenu from "@/components/LeftMenu";
+import RightMenu from "@/components/RightMenu";
+import Image from "next/image";
+import React from "react";
 
 function ProfilePage() {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu type='profile'/>
+        <LeftMenu type="profile" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
-          <Feed/>
+          <div className="flex flex-col items-center justify-between">
+            <div className="w-full h-64 relative">
+              <Image
+                src={
+                  "https://images.pexels.com/photos/2763964/pexels-photo-2763964.jpeg"
+                }
+                alt=""
+                fill
+                className="object-cover"
+              />
+              <Image
+                src={
+                  "https://images.pexels.com/photos/30649518/pexels-photo-30649518/free-photo-of-moody-black-and-white-portrait-of-pensive-man.jpeg"
+                }
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Mike Brown</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">356</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">34k</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">43</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
+          <Feed />
         </div>
       </div>
       <div className="hidden lg:block w-[30%]">
-        <RightMenu userId='test'/>
+        <RightMenu userId="test" />
       </div>
     </div>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
